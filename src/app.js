b45adb026cdd51ad67cfd541242d1f6d1d850310
Braiton2117 <richarctm@gmail.com>
2025-06-08 10:49:26 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,32 +1,35 @@
 import express from 'express'
-import cors from 'cors';//importa los paquetes cors--permisoa de acceso
-import path from 'path';
-import {fileURLToPath} from 'url';
-import clientesRoutes from './routes/clientes.routes.js'
-import usuariosRoutes from './routes/usuarios.routes.js'
-import productosRoutes from './routes/productos.routes.js'
+import cors from 'cors' //importa los paquetes cors-- permisos de acceso
+import path from 'path'
+import { fileURLToPath } from 'url'
+import registrosRoutes from './routes/registros.routes.js'
 
 //definir modulo de ES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app=express();
 const corsOptions={
-    origin:'*',//la direccion del dominio del servidor
-    methods:['GET','POST','PUT','PATCH','DELETE'],
+    origin:'*',//la direccion ip/dominio del servidor
+    methods:['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials:true
 }
-app.use(cors(corsOptions));
+
+app.set('json spaces', 2); // Define la sangría para JSON en modo de desarrollo
+
+app.use(cors(corsOptions))
 app.use(express.json());//para que interprete los objetos json
-app.use(express.urlencoded({extended:true}));//se añade para poder receptar formularios
-app.use('/uploads',express.static(path.join(__dirname,'../uploads')));
+app.use(express.urlencoded({extended:true}));  //se añade para poder receptar formularios
+//app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
+//app.use('/uploads', express.static(path.resolve('uploads')));
 //rutas
-app.use('/api',clientesRoutes)
-app.use('/api',usuariosRoutes )
-app.use('/api',productosRoutes)
+
+app.use('/api',registrosRoutes)
 
 app.use((req,res,next)=>{
     res.status(400).json({
-        message:'Endpoint not found'
+        message: 'Endpoint not found'
     })
 })
 export default app;
